294b563c0235b3c3c9fd1855099de4004db92731
Aleksandr Gyumushyan <alex.gyumushyan@gmail.com>
2024-08-20 18:56:49 -0400

    generated images have noise now

--- validate/shiftImage.py
+++ validate/shiftImage.py
@@ -1,4 +1,22 @@
 from PIL import Image
+import numpy as np
+
+def add_noise(image) -> Image: 
+    img_array = np.array(image)
+
+    mean = 20
+    var = 10
+    sigma = var ** 0.5 
+
+    noise = np.random.normal(mean, sigma, img_array.shape)
+
+    noisy_img_arr = img_array + noise 
+
+    noisy_img_arr = np.clip(noisy_img_arr, 0, 255)
+    noisy_img_arr = noisy_img_arr.astype(np.uint8)
+
+    return Image.fromarray(noisy_img_arr)
+
 
 
 def shift_image(image_path, output_path, x_shift, y_shift): 
@@ -8,6 +26,8 @@ def shift_image(image_path, output_path, x_shift, y_shift):
         shifted = Image.new(img.mode, (width, height))
 
         shifted.paste(img, (x_shift, y_shift))
+
+        shifted = add_noise(shifted)
 
         left = 0
         right = left + width
@@ -27,7 +47,7 @@ def shift_image(image_path, output_path, x_shift, y_shift):
         else: 
             lower += y_shift
 
-        shifted = shifted.crop((left, upper, right, lower))
+        # shifted = shifted.crop((left, upper, right, lower))
 
         shifted.save(output_path)
         print(shifted.size)
@@ -36,8 +56,8 @@ def shift_image(image_path, output_path, x_shift, y_shift):
 
 
 if __name__ == "__main__": 
-    x_shift = 10
-    y_shift = 10
+    x_shift = 1
+    y_shift = 1
     image_path = "/home/alexlinux/projects/StarCounter/testfiles/6stars.png"
     save_path = f"/home/alexlinux/projects/StarCounter/testfiles/6stars_{x_shift}_{y_shift}.png"
 
