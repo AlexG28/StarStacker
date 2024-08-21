90cadce7f5a95c97be73b320896795f9d6ec0c71
Aleksandr Gyumushyan <alex.gyumushyan@gmail.com>
2024-08-21 14:20:40 -0400

    fill transparent pixels with noise

--- validate/shiftImage.py
+++ validate/shiftImage.py
@@ -18,16 +18,19 @@ def add_noise(image) -> Image:
     return Image.fromarray(noisy_img_arr)
 
 
-
-def shift_image(image_path, output_path, x_shift, y_shift): 
+def shift_image(image_path, x_shift, y_shift): 
     with Image.open(image_path) as img: 
         width, height = img.size
-
+        corrected = Image.new("RGB", img.size, (0, 0, 0))
         shifted = Image.new(img.mode, (width, height))
 
         shifted.paste(img, (x_shift, y_shift))
+        
+        corrected.paste(shifted, mask=shifted.split()[3])
 
-        shifted = add_noise(shifted)
+        corrected = add_noise(corrected)
+
+
 
         left = 0
         right = left + width
@@ -48,17 +51,22 @@ def shift_image(image_path, output_path, x_shift, y_shift):
             lower += y_shift
 
         # shifted = shifted.crop((left, upper, right, lower))
+        return corrected
+        
 
-        shifted.save(output_path)
-        print(shifted.size)
-        print(f"image saved to: {output_path}")
+def main(): 
+    x_shift = 5
+    y_shift = 5
+    number_of_images = 1
+    image_path = "/home/alexlinux/projects/StarCounter/testfiles/6stars.png"
+    image_output_dir = "/home/alexlinux/projects/StarCounter/testfiles"
 
+    for i in range(number_of_images): 
+        image = shift_image(image_path, x_shift, y_shift)
+
+        image_name = f"{image_output_dir}/image{i}_{x_shift}_{y_shift}.png" 
+        image.save(image_name)
 
 
 if __name__ == "__main__": 
-    x_shift = 1
-    y_shift = 1
-    image_path = "/home/alexlinux/projects/StarCounter/testfiles/6stars.png"
-    save_path = f"/home/alexlinux/projects/StarCounter/testfiles/6stars_{x_shift}_{y_shift}.png"
-
-    shift_image(image_path, save_path, x_shift, y_shift)
+    main()
